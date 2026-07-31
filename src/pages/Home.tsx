554ddf3226488fa4775d4785a70cadd54efa3ce6
Amirian515 <amir515komak@gmail.com>
2026-07-31 13:48:49 +0330

    rtl Search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,11 +23,12 @@ return(
    <Categories />
 
      <input
+     dir="rtl"
   type="text"
-  placeholder="...جستجوی قهوه"
+  placeholder="جستجوی قهوه . . . "
   value={search}
   onChange={(e) => setSearch(e.target.value)}
-  className="w-full max-w-md mx-auto block mb-8 px-4 py-3 rounded-xl bg-zinc-900 border border-zinc-700 outline-none"
+  className="w-full max-w-md mx-auto block mb-8 px-4 py-3 rounded-xl bg-zinc-900 border border-zinc-700 outline-none placeholder:text-right"
 />
 
    <ProductGrid addToCart={addToCart} search={search}/>
