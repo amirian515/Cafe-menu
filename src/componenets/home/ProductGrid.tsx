3c1269ca771e22f2f74c780ebd90e6b76c2337e4
Amirian515 <amir515komak@gmail.com>
2026-07-31 14:11:56 +0330

    add notfound search

--- src/componenets/home/ProductGrid.tsx
+++ src/componenets/home/ProductGrid.tsx
@@ -7,13 +7,27 @@ function ProductGrid({ addToCart , search}: { addToCart: (product: Product) => v
         );
     return(
         <section className="grid grid-cols-4 gap-3 px-10">
-        {filteredProducts.map((product) => (
+                    { filteredProducts.length === 0 ? (
+
+                <div className="text-center text-white col-span-4 flex flex-col items-center justify-center  mb-50 mt-20">
+                <h2 className="text-2xl font-bold">
+                    😕 محصولی پیدا نشد
+                </h2>
+
+                <p className="text-gray-400 mt-2">
+                    لطفاً عبارت دیگری را جستجو کنید
+                </p>
+                </div>
+
+            ) : (
+        filteredProducts.map((product) => (
             <ProductCard
             key={product.id}
             product={product}
             addToCart={addToCart}
                 />
-         ))}
+         ))
+         )}
         </section>
 
     );
