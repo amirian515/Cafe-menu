ab864cc4b0e9c9ecdda57f37e925ca762d1fd2fa
Amirian515 <amir515komak@gmail.com>
2026-08-09 12:28:03 +0330

    style dashbord

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -11,9 +11,8 @@ const lastOrders = orders.slice(orders.length-5)
   return (
     <section className="my-10 mx-10">
 
-      <div className="text-right border-b border-zinc-700 pb-3">
+      <div className="text-right  pb-3">
         <h1 className="text-3xl mb-5 text-yellow-500">داشبورد </h1>
-        <h3 className="font-bold"> به پنل مدیریت خوش آمدید</h3>
       </div>
       <div className="flex justify-between items-center py-10  gap-5 border-b border-b-zinc-700">
         <div className="flex flex-col items-center gap-5 bg-zinc-900 p-5 rounded-3xl w-full">
@@ -40,17 +39,33 @@ const lastOrders = orders.slice(orders.length-5)
           <p className="w-1/4"> تاریخ</p>
           <p className="w-1/4">شماره</p>
         </div>
+        { lastOrders.length === 0
+        ? <p className="text-center mt-20">سفارشی ثبت نشده</p>
+        :
           <div >
               {lastOrders.map((order) => (
-                  <div key={order.id} className=" flex justify-between pt-5 text-right">
-                    <p className="w-1/4">{order.status}</p>
-                    <p className="w-1/4"> {order.total.toLocaleString()}</p>
+                  <div key={order.id} className=" flex justify-between py-5 text-right border-b border-zinc-800
+                   hover:bg-zinc-800 transition duration-500">
+                      <p
+                        className={`w-1/4 ${
+                          order.status === "در انتظار بررسی"
+                            ? "text-yellow-300"
+                            : order.status === "تکمیل شده"
+                              ? "text-green-400"
+                              : order.status === "لغو شده"
+                                ? "text-red-400"
+                                : "text-white"
+                        }`}
+                      >
+                        {order.status}
+                      </p>
+                    <p className="w-1/4 " dir="ltr"> <span className="mr-1">تومان</span></p>{order.total.toLocaleString()}
                     <p className="w-1/4">{order.date}</p>
-                    <p className="w-1/4">{order.id % 10000}</p>
+                    <p className="w-1/4">{ String(order.id % 10000).padStart(4,"0")}</p>
 
                   </div>
               ))}
-        </div>
+        </div>}
       </div>
 
     </section>
