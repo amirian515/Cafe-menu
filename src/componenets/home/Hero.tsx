b2fedd03e31b93091449cd817004559800d696fa
Amirian515 <amir515komak@gmail.com>
2026-08-22 16:55:46 +0330

    responsive hero

--- src/componenets/home/Hero.tsx
+++ src/componenets/home/Hero.tsx
@@ -4,11 +4,11 @@ import MainImg from "../../assests/images/Hero/MainImg.jpg"
 function Hero(){
     return(
         <section className=" bg-black">
-            <div className="max-w-7xl mx-auto h-full flex justify-between items-center">
-            <div className="w-1/2"><h1 className="text-4xl font-bold">تجربه ای لوکس از طعم و آرامش
+            <div className="max-w-7xl mx-auto h-full flex justify-between items-center  gap-2 md:gap-4 mt-2 text-center">
+            <div className="w-1/2"><h1 className="md:text-4xl font-bold text-right">تجربه ای لوکس از طعم و آرامش
                 </h1>
-                <p className="text-gray-400 mt-4">به کافه لوکس خوش آمدید؛ جایی که هر طعم یک خاطره می‌سازد</p>
-                <button className="bg-yellow-500 text-black font-bold px-6 py-3 rounded-2xl mt-6">سفارش را شروع کنید</button></div>
+                <p className="text-gray-400 mt-4 text-sm md:text-base text-right ">به کافه لوکس خوش آمدید؛ جایی که هر طعم یک خاطره می‌سازد</p>
+                <button className="bg-yellow-500 text-black font-bold md:px-6 md:py-3 px-3 py-2 rounded-2xl mt-6  text-sm md:text-base ">سفارش را شروع کنید</button></div>
                     <div className="w-1/2">
                       <img
                          src= {MainImg}
--- src/componenets/layout/Navbar.tsx
+++ src/componenets/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 function Navbar({ cartCount ,toggleCart }: { cartCount: number; toggleCart :()=>void;  }){
     return(
-        <header className="h-20 border-b border-zinc-800 bg-black sticky top-0 md:static" >
+        <header className="h-20 border-b border-zinc-800 bg-black sticky top-0 md:static z-50" >
             <div className="max-w-7xl mx-auto h-full px-8 flex items-center justify-between ">
                 <h1 className=" md:text-2xl font-bold text-yellow-500"> Cafe luxe</h1>
                 <nav className="flex items-center md:gap-8 gap-4 text-sm md:text-base">
