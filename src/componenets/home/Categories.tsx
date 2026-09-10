b6865baf24c8c51c05a20ebf14cbc404f4c487a3
Amirian515 <amir515komak@gmail.com>
2026-09-11 00:52:52 +0330

    decrese gap

--- src/componenets/home/Categories.tsx
+++ src/componenets/home/Categories.tsx
@@ -29,7 +29,7 @@ function Categories(
     console.log("selectedCategory:", selectedCategory);
     return(
         <section className="max-w-7xl mx-auto px-8 py-10">
-            <div className="grid grid-cols-4 md:grid-cols-4 gap-4">
+            <div className="grid grid-cols-4 md:grid-cols-4  md:gap-4 gap-2">
                 {categories.map((cat)=>(
                     <div
                         key={cat.name}
